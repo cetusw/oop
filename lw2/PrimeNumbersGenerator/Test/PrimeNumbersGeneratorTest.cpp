3884d6faeca7a1eae01d5b17421a14d657c5e591
cetusw <kugelev.m@gmail.com>
2025-03-29 03:24:07 +0300

    PrimeNumbersGenerator Дописал тесты на проверку значений

--- lw2/PrimeNumbersGenerator/Test/PrimeNumbersGeneratorTest.cpp
+++ lw2/PrimeNumbersGenerator/Test/PrimeNumbersGeneratorTest.cpp
@@ -3,7 +3,7 @@
 
 #include <iostream>
 
-void PrimeNumbersGeneratorTest(
+void PrimeNumbersGeneratorCountTest(
 	const int testBound, const int expectedPrimesCount, const std::string& description)
 {
 	SECTION(description)
@@ -12,13 +12,31 @@ void PrimeNumbersGeneratorTest(
 	}
 }
 
-TEST_CASE("PrimeNumbersGeneratorTest")
+void PrimeNumbersGeneratorValueTest(
+	const int testBound, const std::set<int>& expectedPrimes, const std::string& description)
 {
-	PrimeNumbersGeneratorTest(0, 0, "Ноль");
-	PrimeNumbersGeneratorTest(1, 0, "Один");
-	PrimeNumbersGeneratorTest(2, 1, "Два");
-	PrimeNumbersGeneratorTest(10, 4, "Какое-то рандомное число");
-	PrimeNumbersGeneratorTest(100000000, 5761455, "Макс значение");
-	PrimeNumbersGeneratorTest(-1, 0, "Отрицательное значение");
+	SECTION(description) { REQUIRE(GeneratePrimeNumbersSet(testBound) == expectedPrimes); }
+}
 
+TEST_CASE("Count")
+{
+	PrimeNumbersGeneratorCountTest(0, 0, "Ноль");
+	PrimeNumbersGeneratorCountTest(1, 0, "Один");
+	PrimeNumbersGeneratorCountTest(2, 1, "Два");
+	PrimeNumbersGeneratorCountTest(10, 4, "Какое-то рандомное число");
+	PrimeNumbersGeneratorCountTest(100000000, 5761455, "Макс значение");
+	PrimeNumbersGeneratorCountTest(-1, 0, "Отрицательное значение");
+}
+
+TEST_CASE("Value")
+{
+	PrimeNumbersGeneratorValueTest(0, {}, "Ноль");
+	PrimeNumbersGeneratorValueTest(1, {}, "Один");
+	PrimeNumbersGeneratorValueTest(2, { 2 }, "Два");
+	PrimeNumbersGeneratorValueTest(10, { 2, 3, 5, 7 }, "Какое-то рандомное число");
+	PrimeNumbersGeneratorValueTest(-1, {}, "Отрицательное значение");
+	PrimeNumbersGeneratorValueTest(100,
+		{ 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83,
+			89, 97 },
+		"Большое число");
 }
